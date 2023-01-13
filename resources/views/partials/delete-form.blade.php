e7edc2673a9f09a4fd7d74ff38a39cb20740ac13
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-13 17:01:09 +0100

    add-onsubmit-confirm

--- resources/views/partials/delete-form.blade.php
+++ resources/views/partials/delete-form.blade.php
@@ -1,4 +1,5 @@
-<form class="d-inline" action="{{ route('comics.destroy', $item) }}" method="POST">
+<form onsubmit="return confirm('Si sta per eliminare: {{ $item->title }}. Confermare?')" class="d-inline"
+    action="{{ route('comics.destroy', $item) }}" method="POST">
     @csrf
     @method('DELETE')
     <button href="#" class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
